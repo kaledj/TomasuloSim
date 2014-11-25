41e7d923c91bdb5d48504fd1f9dc8b64d23d14f5
kaledj <kaledj@appstate.edu>
2014-11-25 17:28:58 -0500

    Parse command line arguments

--- tomasulo.py
+++ tomasulo.py
@@ -1,5 +1,8 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Simulate execute of DLX code on a Tomasulo processor.')
+parser = argparse.ArgumentParser(description='Simulate execution of DLX code on a Tomasulo processor.')
+parser.add_argument('-f', type=str, dest='filename', required=True, help='The input file. Must be a .hex file.')
+parser.add_argument('-v', '--verbose', action='store_true', help='Increase output verbosity.')
+args = parser.parse_args()
 
